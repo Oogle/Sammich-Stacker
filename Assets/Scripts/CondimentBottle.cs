a693ce23e86011ee965ced8ec114bfa01a19bd40
Ian-Bohanan <84200961+AhoyGamers@users.noreply.github.com>
2024-08-17 17:17:48 -0500

    Removed condimentBottle layers
    Made the condimentBottle and playArea on the same layer since the condimentBottle checks tags now

--- Assets/Scripts/CondimentBottle.cs
+++ Assets/Scripts/CondimentBottle.cs
@@ -8,6 +8,8 @@ public class CondimentBottle : MonoBehaviour
     [SerializeField] private float moveSpeed = 30; //How fast the tween towards the start coordinates should be when player releases mouse button
     [SerializeField] private float spinSpeed = 15; //How fast the rotation tween should play
     [SerializeField] private Transform bottleSprite;
+    [SerializeField] private Transform tip; //The bottle tip transform object that will be used to spray to condiment
+    [SerializeField] private GameObject globsToSpawn; //The prefab to spawn when the player stops holding down the mouse
     private bool movingToStart = false; //Is the bottle trying to go back to the start?
     private bool rotateUp = true; //Should the bottle be rotating up?
     private bool inPlayArea = false; //Is the bottle inside the acceptable play area?
@@ -34,7 +36,7 @@ public class CondimentBottle : MonoBehaviour
     //Sprays the condiment on to the sandwich
     void sprayCondiment()
     {
-
+        movingToStart = true;
     }
 
     //The CondimentBottle layer cannot interact with any other physics layers. This assumes the ONLY other thing on the CondimentBottle layer is the bottleBoundry
